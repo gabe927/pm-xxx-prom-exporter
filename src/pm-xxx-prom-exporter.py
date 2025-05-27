8e408a13472e223d2dd6109d08eb2783cbcb4e03
gabe927 <gabe927@gmail.com>
2025-05-27 01:22:06 -0400

    better way of instantiating gauges

--- src/pm-xxx-prom-exporter.py
+++ src/pm-xxx-prom-exporter.py
@@ -25,40 +25,47 @@ def index():
 log.info("Setting up datapoints...")
 main_label = ["host"]
 
-l1_volt     = Gauge("l1_volt", "Leg 1 Voltage", main_label)
-l2_volt     = Gauge("l2_volt", "Leg 2 Voltage", main_label)
-l3_volt     = Gauge("l3_volt", "Leg 3 Voltage", main_label)
-l12_volt    = Gauge("l12_volt", "Leg 1 to Leg 2 Voltage", main_label)
-l23_volt    = Gauge("l23_volt", "Leg 2 to Leg 3 Voltage", main_label)
-l31_volt    = Gauge("l31_volt", "Leg 3 to Leg 1 Voltage", main_label)
-l1_amps     = Gauge("l1_amps", "Leg 1 Amps", main_label)
-l2_amps     = Gauge("l2_amps", "Leg 2 Amps", main_label)
-l3_amps     = Gauge("l3_amps", "Leg 3 Amps", main_label)
-n_amps      = Gauge("n_amps", "Neutral Leg Amps", main_label)
-frequency   = Gauge("frequency", "Input Frequency", main_label)
-v_avrg      = Gauge("v_avrg", "Average leg voltage", main_label)
-u_avrg      = Gauge("u_avrg", "Average leg-to-leg voltage", main_label)
-i_avrg      = Gauge("i_avrg", "Average current", main_label)
-tot_p       = Gauge("tot_p", "Total Power", main_label)
-tot_q       = Gauge("tot_q", "Total Reactive Power", main_label)
-tot_s       = Gauge("tot_s", "Total Apparent Power", main_label)
-pow_factor  = Gauge("pow_factor", "Power Factor", main_label)
-demand_i1   = Gauge("demand_i1", "Leg 1 Current Demand", main_label)
-demand_i2   = Gauge("demand_i2", "Leg 2 Current Demand", main_label)
-demand_i3   = Gauge("demand_i3", "Leg 3 Current Demand", main_label)
-demand_ia   = Gauge("demand_ia", "Average Current Demand", main_label)
-demand_p    = Gauge("demand_p", "Power Demand", main_label)
-demand_q    = Gauge("demand_1", "Reactive Power Demand", main_label)
-thd_l1      = Gauge("thd_l1", "Leg 1 Voltage Total Harmonic Distortion", main_label)
-thd_l2      = Gauge("thd_l2", "Leg 2 Voltage Total Harmonic Distortion", main_label)
-thd_l3      = Gauge("thd_l3", "Leg 3 Voltage Total Harmonic Distortion", main_label)
-thd_l12     = Gauge("thd_l12", "Leg 1 to Leg 2 Voltage Total Harmonic Distortion", main_label)
-thd_l23     = Gauge("thd_l23", "Leg 2 to Leg 3 Voltage Total Harmonic Distortion", main_label)
-thd_l31     = Gauge("thd_l31", "Leg 3 to Leg 1 Voltage Total Harmonic Distortion", main_label)
-thd_i1      = Gauge("thd_i1", "Leg 1 Current Total Harmonic Distortion", main_label)
-thd_i2      = Gauge("thd_i2", "Leg 2 Current Total Harmonic Distortion", main_label)
-thd_i3      = Gauge("thd_i3", "Leg 3 Current Total Harmonic Distortion", main_label)
-thd_in      = Gauge("thd_in", "Neutral Leg Current Total Harmonic Distortion", main_label)
+gauges = {
+    "up":        {"description": "Is meter up?", "gauge": None},
+    "l1_volt":   {"description": "Leg 1 Voltage", "gauge": None},
+    "l2_volt":   {"description": "Leg 2 Voltage", "gauge": None},
+    "l3_volt":   {"description": "Leg 3 Voltage", "gauge": None},
+    "l12_volt":  {"description": "Leg 1 to Leg 2 Voltage", "gauge": None},
+    "l23_volt":  {"description": "Leg 2 to Leg 3 Voltage", "gauge": None},
+    "l31_volt":  {"description": "Leg 3 to Leg 1 Voltage", "gauge": None},
+    "l1_amps":   {"description": "Leg 1 Amps", "gauge": None},
+    "l2_amps":   {"description": "Leg 2 Amps", "gauge": None},
+    "l3_amps":   {"description": "Leg 3 Amps", "gauge": None},
+    "n_amps":    {"description": "Neutral Leg Amps", "gauge": None},
+    "frequency": {"description": "Input Frequency", "gauge": None},
+    "v_avrg":    {"description": "Average leg voltage", "gauge": None},
+    "u_avrg":    {"description": "Average leg-to-leg voltage", "gauge": None},
+    "i_avrg":    {"description": "Average current", "gauge": None},
+    "tot_p":     {"description": "Total Power", "gauge": None},
+    "tot_q":     {"description": "Total Reactive Power", "gauge": None},
+    "tot_s":     {"description": "Total Apparent Power", "gauge": None},
+    "pow_factor": {"description": "Power Factor", "gauge": None},
+    "demand_i1": {"description": "Leg 1 Current Demand", "gauge": None},
+    "demand_i2": {"description": "Leg 2 Current Demand", "gauge": None},
+    "demand_i3": {"description": "Leg 3 Current Demand", "gauge": None},
+    "demand_ia": {"description": "Average Current Demand", "gauge": None},
+    "demand_p":  {"description": "Power Demand", "gauge": None},
+    "demand_q":  {"description": "Reactive Power Demand", "gauge": None},
+    "thd_l1":    {"description": "Leg 1 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_l2":    {"description": "Leg 2 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_l3":    {"description": "Leg 3 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_l12":   {"description": "Leg 1 to Leg 2 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_l23":   {"description": "Leg 2 to Leg 3 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_l31":   {"description": "Leg 3 to Leg 1 Voltage Total Harmonic Distortion", "gauge": None},
+    "thd_i1":    {"description": "Leg 1 Current Total Harmonic Distortion", "gauge": None},
+    "thd_i2":    {"description": "Leg 2 Current Total Harmonic Distortion", "gauge": None},
+    "thd_i3":    {"description": "Leg 3 Current Total Harmonic Distortion", "gauge": None},
+    "thd_in":    {"description": "Neutral Leg Current Total Harmonic Distortion", "gauge": None},
+}
+
+for k, v in gauges.items():
+    v["Gauge"] = Gauge(k, v["description"], main_label)
+
 
 log.info("Done setting up datapoints")
 
